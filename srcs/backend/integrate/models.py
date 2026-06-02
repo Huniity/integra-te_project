1f147dce665240b683aa7d94b9ce9851d3bd0f9c
Dumitru Cristian <78100242+CrisAxe@users.noreply.github.com>
2026-06-02 20:59:02 +0000

    feat:added class disciplinas

--- srcs/backend/integrate/models.py
+++ srcs/backend/integrate/models.py
@@ -1,3 +1,8 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class disciplinas(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.Charfiled(max_length=200)
+    slug = models.SlugField(max_length=500)
+    desc = models.Charfield(max_length=500)
