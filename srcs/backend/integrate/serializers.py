fdb325d62947c0c0776fbeaa6dcd5b5be7fe2401
Dumitru Cristian <78100242+CrisAxe@users.noreply.github.com>
2026-06-11 20:58:12 +0000

    feat:add serializer Disciplina & Tema

--- srcs/backend/integrate/serializers.py
+++ srcs/backend/integrate/serializers.py
@@ -1,1 +1,21 @@
-# serializers
+from rest_framework import serializers
+from .models import (
+    Disciplina,
+    Tema,
+    Conteudo,  # noqa
+    Jogo,  # noqa
+    Livro,  # noqa
+    MaterialOriginal,  # noqa
+)
+
+
+class DisciplinaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Disciplina
+        fields = "id", "nome", "slug", "desc"
+
+
+class TemaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tema
+        fields = "id", "nome", "slug", "desc"
