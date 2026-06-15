ea28688ea75c0ce5a9c31237e236a05ed7f3abe0
Dumitru Cristian <78100242+CrisAxe@users.noreply.github.com>
2026-06-15 18:41:00 +0000

    feat(views&url): added new views and urls

--- srcs/backend/integrate/urls.py
+++ srcs/backend/integrate/urls.py
@@ -1,1 +1,23 @@
-urlpatterns: list = []
+from rest_framework.routers import DefaultRouter
+
+
+from integrate.views import (
+    DisciplinaViewSet,
+    TemaViewSet,
+    JogoViewSet,
+    LivroViewSet,
+    # ContactoCreateView,
+    # PesquisaView,
+)
+
+
+router = DefaultRouter()
+
+router.register(r"disciplinas", DisciplinaViewSet, basename="disciplinas")
+router.register(r"temas", TemaViewSet, basename="temas")
+router.register(r"jogos", JogoViewSet, basename="jogos")
+router.register(r"livros", LivroViewSet, basename="livros")
+# router.register(r"contactos", ContactoCreateView, basename="contactos")
+# router.register(r"pesquisa", PesquisaView, basename="pesquisa")
+
+urlpatterns = router.urls
