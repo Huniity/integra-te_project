b649e682a5ecc3b7de25c5e35dbab55b175909e3
Dumitru Cristian <78100242+CrisAxe@users.noreply.github.com>
2026-06-15 18:41:00 +0000

    feat(views&url): added new views and urls

--- srcs/backend/integrate/views.py
+++ srcs/backend/integrate/views.py
@@ -1,3 +1,40 @@
-# from django.shortcuts import render
+from rest_framework.views import ReadOnlyModelViewSet  # noqa
+from rest_framework.generics import CreateAPIView  # noqa
 
-# Create your views here.
+from .models import (  # noqa
+    Disciplina,  # noqa
+    Tema,  # noqa
+    Jogo,  # noqa
+    Livro,  # noqa
+)  # noqa
+
+from .serializers import (  # noqa
+    DisciplinaSerializer,  # noqa
+    TemaSerializer,  # noqa
+    JogosSerializers,  # noqa
+    LivroSerializers,  # noqa
+)
+
+
+class DisciplinaViewSet(ReadOnlyModelViewSet):  # noqa
+    queryset = Disciplina.objects.all()  # noqa
+    serializer_class = DisciplinaSerializer  # noqa
+    lookup_field = "slug"  # noqa
+
+
+class TemaViewSet(ReadOnlyModelViewSet):  # noqa
+    queryset = Tema.objects.all()  # noqa
+    serializer_class = TemaSerializer  # noqa
+    lookup_field = "slug"  # noqa
+
+
+class JogoViewSet(ReadOnlyModelViewSet):  # noqa
+    queryset = Jogo.objects.all()  # noqa
+    serializer_class = JogosSerializers  # noqa
+    lookup_field = "slug"  # noqa
+
+
+class LivroViewSet(ReadOnlyModelViewSet):  # noqa
+    queryset = Livro.objects.all()  # noqa
+    serializer_class = LivroSerializers  # noqa
+    lookup_field = "slug"  # noqa
