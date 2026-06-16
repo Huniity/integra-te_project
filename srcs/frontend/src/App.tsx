39715a9f309efab45cef66dc3e474492fe437fd1
DiogoLpes <diogolopes285@gmail.com>
2026-06-16 20:26:06 +0000

    feat-gamespage

--- srcs/frontend/src/App.tsx
+++ srcs/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Resolver from './pages/Resolver'
+import Games from './pages/Jogos'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/exercicios" element={<Resolver />} />
         <Route path="/exercicios/*" element={<Resolver />} />
+        <Route path="/jogos" element={<Games />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
