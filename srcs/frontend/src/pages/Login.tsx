9d686f8a6f53fea0d082f8dd61961c4800e0ebc3
Dumitru Cristian <78100242+CrisAxe@users.noreply.github.com>
2026-06-18 20:56:23 +0000

    feat:editing form background img

--- srcs/frontend/src/pages/Login.tsx
+++ srcs/frontend/src/pages/Login.tsx
@@ -7,19 +7,19 @@ const Login = () => {
       <h1>
         Login Page
       </h1>
-        <img
+        {/* <img
             src="./src/assets/cloud_menu2.png"
             alt=""
             aria-hidden="true"
-            className="z-1 pointer-events-none absolute left-1/2 top-1/2 h-[100%] w-[35%] -translate-x-1/2 -translate-y-1/2 object-fill"
-          />
-      <div className="z-10 flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            className=" z-1 pointer-events-none absolute left-1/2 top-1/2 h-[100%] w-[35%] -translate-x-1/2 -translate-y-1/2 object-fill"
+          /> */}
+      <div className="obrz-10 flex min-h-full flex-col justify-center px-6 py-12 lg:px-3 bg-contain bg-center bg-no-repeat" style={{ backgroundImage: 'url(./src/assets/cloud_menu2.png)' }}>
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm" >
 
             <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
           </div>
 
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm resize"  >
 
             <form action="#" method="POST" className="space-y-6">
               <div>
@@ -64,7 +64,7 @@ const Login = () => {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                  className="resize rounded-md flex w-full justify-center rounded-md bg-indigo-500 px-2if nuvem true detru py-2 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 "
                 >
                   Sign in
                 </button>
